3487bb31ca80b6d3716cd4d216ce22ffd645df6f
vs460 <59885858+vs460@users.noreply.github.com>
2020-01-23 16:28:37 +0000

    Update gen_alpha.m

--- gen_alpha.m
+++ gen_alpha.m
@@ -5,7 +5,7 @@ N = length(beta);
 beta_zf = [beta zeros(1,2^ceil(log2(N)) - N)]; % zero-fill b to the next power of 2 for fft
 B = fft(beta_zf);
 A_mag = sqrt(1 - B.*conj(B));                  % apply magnitude constraint 
-A = exp(ifft(ht(fft(log(A_mag)))));            % apply formula for minimum phase alpha
+A = exp(ifft(hilbert(fft(log(A_mag)))));            % apply formula for minimum phase alpha
 alpha = fft(A)/(2^ceil(log2(N)));              % normalization
 alpha = real(alpha(1:N));                      % use only the original points
 
