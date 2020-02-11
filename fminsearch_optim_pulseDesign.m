53c84e0702f239964bd11a5e9fa70bf919dc850a
vs460 <59885858+vs460@users.noreply.github.com>
2020-02-11 15:23:25 +0000

    Update fminsearch_optim_pulseDesign.m

--- fminsearch_optim_pulseDesign.m
+++ fminsearch_optim_pulseDesign.m
@@ -26,29 +26,28 @@ T1    = 50;                                 % T1 relaxation time for [1-13C]lac
 T2    = 0.2;                                % T2 relaxation time for [1-13C]lac
 
 %% polynomial generation  and iSLR
-beta = firls(N,F,Amp,W);                    % generating linear phase filter
-alpha = gen_alpha(beta);                    % generating matching minimum-phase alpha 
-rf = iSLR(alpha,beta,gamma,TS);             % inverse-SLR transform
+beta    = firls(N,F,Amp,W);                    % generating linear phase filter
+alpha   = gen_alpha(beta);                    % generating matching minimum-phase alpha 
+rf      = iSLR(alpha,beta,gamma,TS);             % inverse-SLR transform
 rf_init = -imag(rf);                        
-tau = time/N;                               % rounding it to the nearest multiple of 4e-6s
-time = length(rf_init)*ceil(tau/4e-6)*4e-6; % 
+tau     = time/N;                               % rounding it to the nearest multiple of 4e-6s
+time    = length(rf_init)*ceil(tau/4e-6)*4e-6; % 
 rf_init = [rf_init;...                      % reshaping the complex pulse to an array of the real and imag part for the optimization
            zeros(1,length(rf_init))];
 %% refine it with optimization
-Nf = 700;                                   % number of frequency points where the result is evaluated in the cost fcn
-ideal_profile = zeros(1,Nf);               
+Nf                     = 700;               % number of frequency points where the result is evaluated in the cost fcn
+ideal_profile          = zeros(1,Nf);               
 ideal_profile(501:end) = sin(45/180*pi);    % design goal specification
-options = optimset('MaxFunEvals',1e8,'MaxIter',1e8,'TolFun',1e-6,'TolX',1e-6);
-pulse = fminsearch(@(pulse) fmin_pulse_design_cost(pulse,ideal_profile,Nf,time,N,T1,T2),rf_init,options);
-pulse_tmp = pulse(1,:) + 1i*pulse(2,:);         % concatenating the real and imag part to a complex waveform
-
+options                = optimset('MaxFunEvals',1e8,'MaxIter',1e8,'TolFun',1e-6,'TolX',1e-6);
+pulse                  = fminsearch(@(pulse) fmin_pulse_design_cost(pulse,ideal_profile,Nf,time,N,T1,T2),rf_init,options);
+pulse_tmp              = pulse(1,:) + 1i*pulse(2,:);         % concatenating the real and imag part to a complex waveform
 
 % evaluate the result at a range of B1 amplitudes
 % for the pre-clinical volume coil used 0.5 - 2 range is reasonable
-df = linspace(-1200,1200,2400);
-B1 = 0.5:0.2:2;
+df    = linspace(-1200,1200,2400);
+B1    = 0.5:0.2:2;
 pulse = B1'*pulse_tmp;
-mx = zeros(length(B1),length(df));my = zeros(length(B1),length(df));mz = zeros(length(B1),length(df));
+mx    = zeros(length(B1),length(df));my = zeros(length(B1),length(df));mz = zeros(length(B1),length(df));
 parfor i = 1:length(B1)
    [mx(i,:),my(i,:),mz(i,:)] = bloch1(pulse(i,:),0,time/Np,T1,T2,df,0,[0 0]);
    while sum(isnan(mx(i,:))+isnan(my(i,:))+isnan(mz(i,:)))>0
