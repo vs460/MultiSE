7145c3edbe1676e729e78c958ebd2ae7d08975f1
vs460 <59885858+vs460@users.noreply.github.com>
2020-02-13 17:41:21 +0000

    Update fminsearch_optim_pulseDesign.m

--- fminsearch_optim_pulseDesign.m
+++ fminsearch_optim_pulseDesign.m
@@ -24,6 +24,9 @@ gamma = 2*pi*10.71e6;                       % 13C gyromagnetic ratio [H/T]
 TS    = 1/f;                                % sampling time of the pulse [s]
 T1    = 50;                                 % T1 relaxation time for [1-13C]lac
 T2    = 0.2;                                % T2 relaxation time for [1-13C]lac
+Nf    = 700;                                % number of frequency points where the result is evaluated in the cost fcn
+FA    = 45;                                 % desired flip-angle
+target_metab = 'lac'                        % target metabolite: 'pyr' or 'lac'
 
 %% polynomial generation  and iSLR
 beta    = firls(N,F,Amp,W);                 % generating linear phase filter
@@ -36,12 +39,18 @@ time    = length(rf_init)*ceil(tau/4e-6)*4e-6;
 rf_init = [rf_init;...                      % reshaping the complex pulse to an array of the real and imag part for the optimization
            zeros(1,length(rf_init))];
 %% refine it with optimization
-Nf                     = 700;               % number of frequency points where the result is evaluated in the cost fcn
-ideal_profile          = zeros(1,Nf);               
-ideal_profile(501:end) = sin(45/180*pi);    % design goal specification
-options                = optimset('MaxFunEvals',1e8,'MaxIter',1e8,'TolFun',1e-6,'TolX',1e-6);
-pulse                  = fminsearch(@(pulse) fmin_pulse_design_cost(pulse,ideal_profile,Nf,time,N,T1,T2),rf_init,options);
-pulse_tmp              = pulse(1,:) + 1i*pulse(2,:);         % concatenating the real and imag part to a complex waveform
+if strcmp(target_metab,'lac')
+    df      = linspace(-1200,200,Nf);           % freqeuncy range where the profiles are evaluated
+elseif strcmp(target_metab,'pyr')
+    df      = linspace(-200,1200,Nf);           % freqeuncy range where the profiles are evaluated
+else
+    error("Unrecognized metabolite! Use either 'lac' or 'pyr' \n")
+end   
+ideal_profile                  = zeros(1,Nf);               
+ideal_profile(abs(df)<inhomHz) = sin(FA/180*pi);    % design goal specification
+options                        = optimset('MaxFunEvals',1e8,'MaxIter',1e8,'TolFun',1e-6,'TolX',1e-6);
+pulse                          = fminsearch(@(pulse) fmin_pulse_design_cost(pulse,ideal_profile,Nf,time,N,T1,T2,inhomHz,target_metab),rf_init,options);
+pulse_tmp                      = pulse(1,:) + 1i*pulse(2,:);         % concatenating the real and imag part to a complex waveform
 
 %% evaluate the result at a range of B1 amplitudes, for the pre-clinical volume coil used 0.5 - 2 range is reasonable
 df    = linspace(-1200,1200,2400);
