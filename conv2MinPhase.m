afb6f5874c865651151b0a9a050bc847e326a094
vs460 <59885858+vs460@users.noreply.github.com>
2020-03-05 11:42:37 +0000

    Update conv2MinPhase.m

--- conv2MinPhase.m
+++ conv2MinPhase.m
@@ -28,7 +28,6 @@ x         = sym('x');
 yourpoly  = expand(prod(x-v));                                    % building the final polynomial from the roots
 b_fir_tmp = sym2poly(yourpoly);                                   % convertig back from symbolic 
 b_fir     = b_fir_tmp;
-b_zeros   = roots(b_fir);                                         % check the result once again
 zplane(b_fir);
 title('Zero-pole plot of the result to check numerical stability of the root search')
 pause()
